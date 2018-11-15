deb7d1745714941cb82db97ea7dbc5086f29e108
Prabeen <prabeensoreng2119@gmail.com>
2018-11-16 01:16:12 +0530

    applying data binding and filtering for customers component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
+import { CapitalizePipe } from "./capilatize.pipe";
 
 
 @NgModule({
-    declarations: [],
-    exports: []
+    declarations: [CapitalizePipe],
+    exports: [CapitalizePipe]
 })
 export class SharedModule{}
